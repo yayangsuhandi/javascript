b8c9b2ea528dbb88ff04c7704370149f0b9d8361
yayangsuhandi <yayangsuhandi@gmail.com>
2015-11-21 11:32:24 +0700

    Create embedtv.js

---
+++ embedtv.js
@@ -0,0 +1,3 @@
+// JScript File
+document.write('<ifr'+'ame src="http://live-player-tv.blogspot.co.id/p/='+ chtv +'" width='+ v_width +' height=' + v_height + ' marginheight="0" marginwidth="0" scrolling="no" border="0" frameborder="0" allowfullscreen ></ifr'+'ame>') ;
+/**/
